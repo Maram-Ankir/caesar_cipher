623ad89ebde8007a430e080aaddc55928410c2c8
Maram-Ankir <maramankir5@gmail.com>
2021-08-28 11:31:48 +0300

    edit the lab

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,40 +1,56 @@
+import nltk
+import re
 
-from cryptography.fernet import Fernet
-import cryptography
-
-
-def encrypt(plain, key):
-    encrypted_text = ""
-
-    for char in plain:
-
-        if (char.isupper()):
-            if char.isalpha() == False:
-                encrypted_text +=" "
-            else:
-                encrypted_text += chr((ord(char) + key-65) % 26 + 65)
-  
+def encrypt( plain_text, key ):
+    encrypted_msg = ""
+    for letter in plain_text:
+        if letter.isupper():  
+            new_letter = chr((ord( letter ) - ord( 'A' )+ key) % 26 + ord( 'A' ))
+            encrypted_msg = encrypted_msg + new_letter
+        elif letter.islower(): 
+            new_letter = chr((ord( letter ) - ord( 'a' )+ key) % 26 + ord( 'a' ))
+            encrypted_msg = encrypted_msg + new_letter
+        elif letter.isdigit():
+            new_numb = ( int( letter ) + key ) % 10
+            encrypted_msg = encrypted_msg + str( new_numb )
         else:
-            if char.isalpha()== False:
-                encrypted_text +=" "
-            else:
-                encrypted_text += chr((ord(char) + key - 97) % 26 + 97)
-      
+            encrypted_msg = encrypted_msg + letter
 
-    return encrypted_text
+    return encrypted_msg
+
+def decrypt( encrypted_msg, key ):
+    return encrypt( encrypted_msg, -key )
 
 
-def decrypt(encoded, key):
-    return encrypt(encoded, -key)
+nltk.download('names')
+nltk.download('words')
+from nltk.corpus import words, names
+word_list = words.words()
+name_list = names.words()
 
-def crack(message):
-    key = Fernet.generate_key()
-    fernet = Fernet(key)
-    encMessage = fernet.encrypt(message.encode())
-    print("original string: ", message)
-    print("encrypted string: ", encMessage)
-    decMessage = fernet.decrypt(encMessage).decode()
-  
-    return( decMessage )
+def count_words(text):
+    candidate_words = text.split()
+    word_count = 0
+    for candidate in candidate_words:
+        word = re.sub(r'[^A-Za-z]+','', candidate)
+        if word.lower() in word_list or word in name_list:
+             # print("english word", word)
+            word_count += 1
+        else:
+            pass
+    return word_count
+
+def crack( encrypted_msg_msg ):
+    decrypted_msg = ''
+    for key in range(26):
+        expected = decrypt(encrypted_msg_msg, key)
+        word_count = count_words(expected)
+        percent = int(word_count / len(expected.split()) * 100)
+    if not decrypted_msg :
+        decrypted_msg = "Not an English text."
+        if percent > 50:
+            decrypted_msg = expected
+
+    return decrypted_msg
 
 
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -7,32 +7,22 @@ def test_version():
     assert __version__ == '0.1.0'
 
 
-def test_encrypt_string_with_given():
-
+def test_encrypt_string():
     assert encrypt("abc", 1) =="bcd"
 
-
 def test_decrypt_previously_encrypted_string_same_shift():
-    orginal_text="abc"
-    encrypt_text=encrypt(orginal_text, 1)
+    encrypt_text=encrypt('abc', 1)
     decrypt_test=decrypt(encrypt_text, 1)
+    assert 'abc' == decrypt_test
 
-    assert orginal_text == decrypt_test
-
-def test_encryption_should_handle_upper_and_lower_case_letters():
-
+def test_handle_upper_and_lower_case_letters():
      assert encrypt("AbC", 1) =="BcD"
 
+def test_allow_non_alpha_characters_but_ignore():
+    assert encrypt("ab c", 1)=="bc d"
+    assert encrypt("ab##c", 1)=="bc##d"
 
-def test_encryption_should_allow_non_alpha_characters_but_ignore():
-
-    assert encrypt("a@bc", 1)=="b cd"
-
-
-
-def test_decrypt_encrypted_version():
-
-    original_text="Maram"
-    crack_test=(crack(original_text))
-    
-    assert crack_test == original_text
+def test_crack_not_50per_english_text():
+    encrypt_text = encrypt( "maram", 1 )
+    actual = crack( encrypt_text )
+    assert actual == "Not an English text."
